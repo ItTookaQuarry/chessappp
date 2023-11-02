a229a1d73d1c4c9f41723f5616af1a304bc94c27
Przemek Konieczny <przemyslawkoniecznyy@gmail.com>
2023-11-02 20:48:36 +0100

    new commit

--- app/(components)/Roomsclient.js
+++ app/(components)/Roomsclient.js
@@ -1,7 +1,7 @@
 "use client"
 import Link from 'next/link'
 import React from 'react'
-import Room from "./Room";
+import Room from "./Room" ;
 export default function Roomsclient(props) {
 
 
@@ -10,7 +10,7 @@ export default function Roomsclient(props) {
 
   return (
     <div className='grid  w-screen relative' >
-    <div className='grid col-span-full m-auto lg:h-2/3 w-full h-full'>{props.data.map((each,index)=>{
+    <div className='grid col-span-full m-auto lg:h-2/3 w-full h-full gap-10'>{props.data.map((each,index)=>{
         return (
            <Room data={each} index={index} key={index} />
         )
