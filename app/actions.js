914087b37c3bee6836a5b946b9046ff07e805818
Przemek Konieczny <przemyslawkoniecznyy@gmail.com>
2023-11-19 16:10:09 +0100

    new commit

--- app/actions.js
+++ app/actions.js
@@ -4,18 +4,19 @@ import { cookies } from 'next/headers'
 import { revalidatePath } from "next/cache";
 import { db } from "./(firebase)/firebase";
 export async function myAction(FormData) {
-    const desc=FormData.get("desciption")
-    const cookieStore = cookies()
-    const email = cookieStore.get('email')?.value
-    const docRef = doc(db,"users",`${email}`);
+    return null
+//     const desc=FormData.get("desciption")
+//     const cookieStore = cookies()
+//     const email = cookieStore.get('email')?.value
+//     const docRef = doc(db,"users",`${email}`);
 
 
-await updateDoc(docRef,{
-    Description:desc
-}) 
+// await updateDoc(docRef,{
+//     Description:desc
+// }) 
 
     
 
 
-revalidatePath('/account') 
+// revalidatePath('/account') 
 }
