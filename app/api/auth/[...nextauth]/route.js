8b6f9ed83fdd7c2985a796f7d4fc680cd3f7263c
Przemek Konieczny <przemyslawkoniecznyy@gmail.com>
2023-11-23 21:12:37 +0100

    new commit

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,22 +2,9 @@
 
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
-import  EmailProvider  from "next-auth/providers/email";
+
 
 const handler = NextAuth({providers: [
-
-
-  EmailProvider({
-    server: {
-      host: process.env.EMAIL_SERVER_HOST,
-      port: process.env.EMAIL_SERVER_PORT,
-      auth: {
-        user: process.env.EMAIL_SERVER_USER,
-        pass: process.env.EMAIL_SERVER_PASSWORD
-      }
-    },
-    from: process.env.EMAIL_FROM
-  }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
